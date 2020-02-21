aa4c653f0afb8c0684b671a2ec88391b341860a7
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-22 00:32:59 +0300

    add new styles

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".imageIconDesign").click(function(){
+        $(".imageIconDesign").toggle();
+        $("p").toggle();
+    });
+    
+    $(".imageIconPro").click(function(){
+        $("p").toggle()
+    });
+    $()
+});
+$(document).ready(function(){
+    $(".imageIconDev").click(function(){
+        $("p").toggle()
+    });
+})
