0fd9eabe25c224bdc899bca279557dc5e77f54e4
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-24 12:10:19 +0300

    add mew styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,57 @@
 $(document).ready(function(){
-    $(".imageIconDesign").click(function(){
+    $(".designPar").click(function(){
         $(".imageIconDesign").toggle();
-        $("p").toggle();
+        $(".design").toggle();
+    });
+    $(".developmentPar").click(function(){
+        $( ".imageIconDev").toggle();  
+        $(".dev").toggle();
     });
     
-    $(".imageIconPro").click(function(){
-        $("p").toggle()
-    });
-    $()
+        $(".prod").click(function(){
+            $(".imageIconPro").toggle();
+            $(".product").toggle();
+        });
+        //$(".works").hover(function (){
+           // $(this).find("hoverCaption").fadeIn(60);
+        //}, function(){
+         //   $(this).find(".hoverCaption").fadeOut(30)
+       // });
+
+        $(".works44").hover(function (){
+            $('.work4').toggle();
+        });
+        $(".works33").hover(function(){
+            $(".work3").toggle();
+        });
+        $(".works22").hover(function(){
+            $(".work2").toggle();
+        });
+        $(".works11").hover(function(){
+            $(".work1").toggle();
+        });
+        $(".works55").hover(function(){
+            $(".work5").toggle();
+        });
+        $(".works66").hover(function(){
+            $(".work6").toggle();
+        });
+        $(".works77").hover(function(){
+            $(".work7").toggle();
+        });
+        $(".works88").hover(function(){
+            $(".work8").toggle();
+        });
+
+        $("form#form").submit(function(){
+            var name = document.getElementById("name").value
+            var email = document.getElementById ("email").value
+             if (name === "" || email === ""){
+                 alert ("Kindly fill in your details")
+             } else 
+             {
+                alert("Thank you very much "+ name  +"  your message has been sent.")
+             }
+        })
+
 });
-$(document).ready(function(){
-    $(".imageIconDev").click(function(){
-        $("p").toggle()
-    });
-})
